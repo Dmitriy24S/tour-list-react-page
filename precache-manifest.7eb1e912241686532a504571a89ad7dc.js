4d532de4cc1687d39109d9d500d51ed90a32b610
Dmitriy <devilins24@gmail.com>
2021-09-17 12:12:58 +0300

    Updates

---
+++ precache-manifest.7eb1e912241686532a504571a89ad7dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d9b5d0b29d09579bf869f4c5f40407",
+    "url": "/tour-list-react-page/index.html"
+  },
+  {
+    "revision": "b767e20a637c9665cd48",
+    "url": "/tour-list-react-page/static/css/main.84a6f634.chunk.css"
+  },
+  {
+    "revision": "2870952f5348e8ade3fb",
+    "url": "/tour-list-react-page/static/js/2.aed9be0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tour-list-react-page/static/js/2.aed9be0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b767e20a637c9665cd48",
+    "url": "/tour-list-react-page/static/js/main.327a02e1.chunk.js"
+  },
+  {
+    "revision": "fdc86ee4893cb967d9f7",
+    "url": "/tour-list-react-page/static/js/runtime-main.79b14590.js"
+  }
+]);
